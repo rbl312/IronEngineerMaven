240ef83e77e1654ca304ad229b1c861ed0dd62c2
gshappell1 <Goldeagle45#>
2019-12-01 20:53:48 -0800

    commenting and styling

--- src/main/java/com/COEN174/IronEngineer/repositories/CompetitorRepository.java
+++ src/main/java/com/COEN174/IronEngineer/repositories/CompetitorRepository.java
@@ -3,7 +3,14 @@ package com.COEN174.IronEngineer.repositories;
 import org.springframework.data.repository.CrudRepository;
 import com.COEN174.IronEngineer.entities.Competitor;
 
-
+// Interface Name: CompetitorRepository
+// Interface Description: CRUD (Create Read Update Delete) repository implementation for the Competitor class
 public interface CompetitorRepository extends CrudRepository<Competitor,Integer> {
+
+    // Function Name: findByEmail
+    // Function Parameters: email (String), the email of the competitor to be searched for
+    // Expected Results: The given competitor is returned by searching the database for the competitor's email.
+    // Function Description: Searches the Iron Engineer database for the provided email and returns a competitor if the email is registered in the system.
+    // Notes: The implementation of this function is handled by the Spring Framework
     public Competitor findByEmail(String email);
 }
--- src/main/java/com/COEN174/IronEngineer/repositories/TeamRepository.java
+++ src/main/java/com/COEN174/IronEngineer/repositories/TeamRepository.java
@@ -6,6 +6,17 @@ import com.COEN174.IronEngineer.entities.Team;
 import java.util.List;
 
 public interface TeamRepository extends CrudRepository<Team,Integer>{
+    // Function Name: findByTeamName
+    // Function Parameters: teamName (String), the team name of the team to be searched for
+    // Expected Results: The given team is returned by searching the database for the team's team name.
+    // Function Description: Searches the Iron Engineer database for the provided team name and returns a competitor if the team name is registered to a team in the system.
+    // Notes: The implementation of this function is handled by the Spring Framework
     public Team findByTeamName(String teamName);
+
+    // Function Name: findByTeamId
+    // Function Parameters: id (Integer), the key of the team to be searched for
+    // Expected Results: The given team is returned by searching the database for the team's primary key.
+    // Function Description: Searches the Iron Engineer database for the provided id number and returns a team if the id is registered to a team in the system.
+    // Notes: The implementation of this function is handled by the Spring Framework
     public Team findByTeamId(Integer id);
 }
